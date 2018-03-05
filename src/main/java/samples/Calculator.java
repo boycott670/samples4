7a59864c724c6f889daaafe45f171531825a4639
marsouf <marsouf224@gmail.com>
2018-03-05 13:26:15 +0000

    MINUS_OPERATOR

--- src/main/java/samples/Calculator.java
+++ src/main/java/samples/Calculator.java
@@ -1,5 +1,7 @@
 package samples;
 
+import java.util.Arrays;
+
 public final class Calculator extends Object
 {
 	public final String eval (final String input)
@@ -7,6 +9,13 @@ public final class Calculator extends Object
 		if (input == null || input.isEmpty())
 		{
 			return "";
+		}
+		
+		if (input.contains("-") && !input.substring(input.indexOf("-") + 1).contains("-"))
+		{
+			final int[] operands = Arrays.stream(input.split("\\-")).map(String::trim).mapToInt(Integer::valueOf).toArray();
+			
+			return String.valueOf(operands[0] - operands[1]);
 		}
 		
 		throw new UnsupportedOperationException("Not yet implemented !");
